036015c983a0360fc16f85ef23da962929c9cf4a
TheoL9 <theodor.lakovic@ndus.edu>
2024-11-20 14:44:14 -0600

    New Class - Student.cs

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Lakovic_nov20_cl2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastDescription { get; set; }
+    }
+}
